e2c9e21ab4bfc966e5abee403d98b8e7f33b4085
iwpeifer <iwpeifer@gmail.com>
2017-04-19 09:14:24 -0400

    added logo

--- bin/run.rb
+++ bin/run.rb
@@ -1,6 +1,13 @@
 require_relative '../config/environment.rb'
 require_relative '../yelpmealee.rb'
 
+puts ' __  __            _           '.red
+puts '|  \/  |          | |          '.red
+puts '| \  / | ___  __ _| | _'.red + '✔'.green + '_  ___ '.red
+puts '| |\/| |/ _ \/ _  | |/ _ \/ _ \.'.red
+puts '| |  | |  __/ (_| | |  __/  __/'.red
+puts '|_|  |_|\___|\__,_|_|\___|\___|'.red
+
 puts "Welcome to Mealee!"
 puts "Please enter your " + "zip code".blue + ":"
 location = gets.chomp
