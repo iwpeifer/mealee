e9b0ab9b7028cd7faad13eed4da66172ef67f6d8
iwpeifer <iwpeifer@gmail.com>
2017-04-19 13:40:00 -0400

    added Yelp credit

--- bin/run.rb
+++ bin/run.rb
@@ -1,7 +1,6 @@
 require_relative '../config/environment.rb'
 require_relative '../lib/yelpmealee.rb'
 
-
 puts ' __  __            _           '.red
 puts '|  \/  |          | |          '.red
 puts '| \  / | ___  __ _| | _'.red + '✔'.green + '_  ___ '.red
@@ -9,13 +8,13 @@ puts '| |\/| |/ _ \/ _  | |/ _ \/ _ \.'.red
 puts '| |  | |  __/ (_| | |  __/  __/'.red
 puts '|_|  |_|\___|\__,_|_|\___|\___|'.red
 
-puts "Welcome to Mealee!"
+puts "Welcome to Mealee!".red + " Powered by Yelp."
 puts "Please enter your " + "zip code".blue + ":"
-#binding.pry
 location = gets.chomp
-puts " "
 puts "What are you lookin' for?"
 term = gets.chomp
+#binding.pry
 
 new_game = Mealee.new
 new_game.search(term, location)
+
