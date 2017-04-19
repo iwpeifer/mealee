41a82a31797e9ffa005a4c1752c107b606bbe0e8
github username <github email address>
2017-04-19 16:59:01 -0400

    added user database functionality and refactored run file

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,28 @@ has_many :losers
 has_many :restaurants, through: :winners
 has_many :restaurants, through: :losers
 
+def user_location
+ 	if self.location == nil
+ 		# initial_location(user)
+ 		# binding.pry
+ 	puts " "
+	puts "Please enter your " + "zip code".blue + ":"
+	location = gets.chomp
+	self.location = location
+	self.save
+ 	else
+ 		# existing_location(user)
+ 	puts "Use previous location? (Previous location = #{self.location}). If yes, press ENTER; if no, enter new location below."
+	answer = gets.chomp
+	if answer == ""
+		location = self.location
+	else
+		location = answer
+		self.location = location
+		self.save
+	end
+	end
+end
+
 end
 
--- bin/run.rb
+++ bin/run.rb
@@ -1,21 +1,11 @@
 require_relative '../config/environment.rb'
 require_relative '../lib/yelpmealee.rb'
 
-
-puts ' __  __            _           '.red
-puts '|  \/  |          | |          '.red
-puts '| \  / | ___  __ _| | _'.red + '✔'.green + '_  ___ '.red
-puts '| |\/| |/ _ \/ _  | |/ _ \/ _ \.'.red
-puts '| |  | |  __/ (_| | |  __/  __/'.red
-puts '|_|  |_|\___|\__,_|_|\___|\___|'.red
-
-puts "Welcome to Mealee!"
-puts "Please enter your " + "zip code".blue + ":"
-#binding.pry
-location = gets.chomp
-puts " "
-puts "What are you lookin' for?"
-term = gets.chomp
-
+CLI.intro_image
+name = CLI.enter_name
+u = User.find_or_create_by(name: name)
+user_location = u.user_location
+user_term = CLI.enter_term
 new_game = Mealee.new
-new_game.search(term, location)
+new_game.search(user_term, user_location)
+
